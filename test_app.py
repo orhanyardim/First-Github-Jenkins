aa7b80a862b26b21317e7196a9f7d78bb2717212
ORHAN YARDİM <100990779+orhanyardim@users.noreply.github.com>
2024-10-31 08:56:26 +0300

    Update test_app.py
    Added a try catch block appropriate to the test result conditions and displayed the test result on the screen.

--- test_app.py
+++ test_app.py
@@ -1,10 +1,10 @@
-import unittest
-from app import add
-
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        try:
+            self.assertEqual(add(2, 3), 5)
+            print("Test Passed: add(2, 3) == 5")
+        except AssertionError:
+            print("Test Failed: add(2, 3) != 5")
 
 if __name__ == '__main__':
     unittest.main()
-
